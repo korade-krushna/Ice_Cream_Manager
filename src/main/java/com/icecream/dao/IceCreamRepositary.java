0bf24880edf61a1a759c8db03fbf5b5f7baa3b23
korade-krushna <101387736+korade-krushna@users.noreply.github.com>
2022-06-03 04:48:39 +0530

    1

--- src/main/java/com/icecream/dao/IceCreamRepositary.java
+++ src/main/java/com/icecream/dao/IceCreamRepositary.java
@@ -1,9 +1,13 @@
 package com.icecream.dao;
 
+import com.icecream.models.Employee;
 import com.icecream.models.IceCream;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface IceCreamRepositary extends JpaRepository<IceCream, Integer> {
 
-
+    @Query(value = "select u from Employee u where u.name =:name")
+    IceCream getIceCreambyName(@Param("name") String name);
 }
